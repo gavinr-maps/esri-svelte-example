f4b350c1299533f6182455cfd1487fd8b391b877
Gavin Rehkemper <gavinr@users.noreply.github.com>
2021-09-09 15:04:45 +0000

    example of using vite (#13)
    * example of using vite
    
    * simplify CSS and async
    
    * css explaination

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
-# esri-svelte-example
+# Esri Svelte Example
 
-An example [Svelte](https://svelte.dev/) application that shows how to use [esri-loader](https://github.com/Esri/esri-loader) to load a map with the [ArcGIS API for JavaScript](https://developers.arcgis.com/javascript/). 
+An example [Svelte](https://svelte.dev/) application that shows how use the [ArcGIS API for JavaScript](https://developers.arcgis.com/javascript/) to load a map.
 
-[View the live app here](https://esri-svelte-example.surge.sh/).
+[View the live app here](https://esri-svelte-example.gavinr.com/).
 
-## Get Started - CodeSandbox
+## Get Started
 
-Open [this template](https://codesandbox.io/s/esri-svelte-example-syije) on CodeSandbox, click "Fork", and start tinkering!
+To get started, follow these instructions.
 
-## Get Started - Local
+1. Run the [Vite init workflow](https://vitejs.dev/guide/#scaffolding-your-first-vite-project) choosing the Svelte template:
 
-1. Clone this repo
-2. `npm install`
-3. `npm run dev`
-4. Open a browser to `https://localhost:5000`
+    ```bash
+    npm init vite@latest esri-svelte-project --template svelte
+    ```
+
+    or if you prefer TypeScript:
+
+    ```bash
+    npm init vite@latest esri-svelte-project --template svelte-ts
+    ```
+
+2. Then install the dependencies and run:
+
+    ```bash
+    cd esri-svelte-project
+    npm install
+    npm install @arcgis/core --save-dev
+    npm run dev
+    ```
+
+3. Copy contents of `App.svelte` in this repository into your `App.svelte`.
 
 ## Links
 
-- [esri-loader](https://github.com/Esri/esri-loader)
 - [ArcGIS API for JavaScript](https://developers.arcgis.com/javascript/)
 - [Svelte with the ArcGIS API for JavaScript](https://odoe.net/blog/svelte-with-the-arcgis-api-for-javascript/)
 - [Esri Svelte Basemaps Example](https://github.com/jwasilgeo/esri-svelte-basemaps-example)
--- src/App.svelte
+++ src/App.svelte
@@ -1,61 +1,56 @@
 <script>
-  import { loadModules } from "esri-loader";
+  import Map from "@arcgis/core/Map";
+  import MapView from "@arcgis/core/views/MapView";
 
-  export let title;
+  /**
+   * This includes the CSS from the ArcGIS API for JavaScript
+   * You can alternativly do this in the style tag below:
+   *
+   * ```
+   * @import "@arcgis/core/assets/esri/themes/light/main.css";
+   * ```
+   *
+   * ... Vite includes it in the same way - so use either way you prefer.
+   *
+   * Also note: you can use the "light" or "dark" theme here. See:
+   * https://developers.arcgis.com/javascript/latest/styling/
+   */
+  import "@arcgis/core/assets/esri/themes/light/main.css";
+
   export let centerText;
-
   // Function that gets called when the element is created.
   // https://svelte.dev/tutorial/actions
   // https://svelte.school/tutorials/introduction-to-actions
-  const createMap = async (domNode) => {
-    // Use esri-loader to load the EsriMap and MapView modules
-    // // https://github.com/Esri/esri-loader#usage
-    const esriLoaderOptions = { css: true };
-    const [EsriMap, MapView] = await loadModules(
-      ["esri/Map", "esri/views/MapView"],
-      esriLoaderOptions
-    );
-
+  const createMap = (domNode) => {
     // Create the map
-    const map = new EsriMap({
-      basemap: "streets"
+    const map = new Map({
+      basemap: "streets",
     });
-
     // Create the mapView from the map
     const view = new MapView({
       container: domNode,
       map: map,
       zoom: 8,
-      center: [-90, 38] // longitude, latitude
+      center: [-90, 38], // longitude, latitude
     });
-
     // Use the watch functionality of the JavaScript API (view.watch) to call a
     // function every time the extent changes. Every time it does, update the
     // "centerText" variable - Svelte takes care of updating the UI based
     // on this variable assignment
     // (Reactivity!) https://svelte.dev/tutorial/reactive-assignments
-    view.watch("center", center => {
+    view.watch("center", (center) => {
       const { latitude, longitude } = center;
       centerText = `Lat: ${latitude.toFixed(2)} | Lon: ${longitude.toFixed(2)}`;
     });
   };
 </script>
 
-<style>
-  h1 {
-    color: purple;
-  }
-  .view {
-    height: 400px;
-    width: 400px;
-  }
-</style>
-
-<h1>{title}</h1>
+<h1>Esri Svelte Example</h1>
 <p>
   An example
   <a href="https://svelte.dev/">Svelte</a>
-  application that shows how to use esri-loader to load a map. Check out the
+  application that shows how use the ArcGIS API for JavaScript to load a map. Check
+  out the
   <a href="https://github.com/gavinr/esri-svelte-example">code</a>
   for more info!
 </p>
@@ -68,3 +63,13 @@
 {#if centerText}
   <p>{centerText}</p>
 {/if}
+
+<style>
+  /* Alternative to the CSS import in the script tag above: */
+  /* @import "@arcgis/core/assets/esri/themes/light/main.css"; */
+
+  .view {
+    height: 400px;
+    width: 400px;
+  }
+</style>
