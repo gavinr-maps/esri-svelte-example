d4e51657b5e2b85e0fdc5ebb4a49649e5de7a9bc
jwasilgeo <jwasilkowski@esri.com>
2021-12-10 15:01:29 -0600

    locally-managed ArcGIS API for JavaScript assets

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// ArcGIS API config
+import esriConfig from "@arcgis/core/config.js";
+esriConfig.assetsPath = "./arcgis-assets";
+
+// Svelte entry point
 import App from "./App.svelte";
 
 const app = new App({
