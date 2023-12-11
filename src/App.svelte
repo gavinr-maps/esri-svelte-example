fcfd2c8fd48480667656bf0f383b81ffa98f45a5
Gavin Rehkemper <grehkemper@esri.com>
2023-12-11 15:17:07 -0600

    convert to arcgis-components

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import Map from "@arcgis/core/Map";
-  import MapView from "@arcgis/core/views/MapView";
+  import "@arcgis/map-components/dist/components/arcgis-map";
+
   /**
    * This includes the CSS from the ArcGIS API for JavaScript
    * You can alternatively do this in the style tag below:
@@ -15,32 +15,28 @@
    * https://developers.arcgis.com/javascript/latest/styling/
    */
   import "@arcgis/core/assets/esri/themes/light/main.css";
-  export let centerText;
-  // Function that gets called when the element is created.
-  // https://svelte.dev/tutorial/actions
-  // https://svelte.school/tutorials/introduction-to-actions
-  const createMap = (domNode) => {
-    // Create the map
-    const map = new Map({
-      basemap: "streets-vector",
+
+  let zoom = 14;
+  let arcgisMap;
+
+  let center;
+
+  $: centerText = center
+    ? `Lat: ${center.latitude.toFixed(3)} | Lon: ${center.longitude.toFixed(3)}`
+    : "";
+
+  $: if (arcgisMap) {
+    arcgisMap.addEventListener("viewReady", (evt) => {
+      // when the view is ready, we can do use it:
+      const view = evt.target.view;
+
+      center = view.center;
+
+      view.watch("center", (c) => {
+        center = c;
+      });
     });
-    // Create the mapView from the map
-    const view = new MapView({
-      container: domNode,
-      map: map,
-      zoom: 14,
-      center: [-90.188, 38.625], // longitude, latitude
-    });
-    // Use the watch functionality of the JavaScript API (view.watch) to call a
-    // function every time the extent changes. Every time it does, update the
-    // "centerText" variable - Svelte takes care of updating the UI based
-    // on this variable assignment
-    // (Reactivity!) https://svelte.dev/tutorial/reactive-assignments
-    view.watch("center", (center) => {
-      const { latitude, longitude } = center;
-      centerText = `Lat: ${latitude.toFixed(3)} | Lon: ${longitude.toFixed(3)}`;
-    });
-  };
+  }
 </script>
 
 <h1>Esri Svelte Example</h1>
@@ -56,7 +52,14 @@
 <!-- use:createMap calls the "createMap" function (defined above) when the  -->
 <!-- element is created. -->
 <!-- See the "createMap" function def above for more info. -->
-<div class="view" use:createMap />
+<div class="mapWrapper">
+  <arcgis-map
+    basemap="streets-vector"
+    {zoom}
+    center={[-90.188, 38.625]}
+    bind:this={arcgisMap}
+  />
+</div>
 
 {#if centerText}
   <p>{centerText}</p>
@@ -65,7 +68,8 @@
 <style>
   /* Alternative to the CSS import in the script tag above: */
   /* @import "@arcgis/core/assets/esri/themes/light/main.css"; */
-  .view {
+
+  .mapWrapper {
     height: 400px;
     width: 600px;
   }
